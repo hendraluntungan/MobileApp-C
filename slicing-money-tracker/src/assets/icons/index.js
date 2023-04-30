0da65ac35020850f98a5f5f44de5806e319a6c0d
Hendra Luntungan <90225495+hendraluntungan@users.noreply.github.com>
2023-04-30 16:12:58 +0800

    Delete slicing-money-tracker directory

--- slicing-money-tracker/src/assets/icons/index.js
+++
@@ -1,7 +0,0 @@
-import Logo from './Logo.svg';
-import IconBack from './icon-back.svg';
-import HomeIcon from './Subtract.svg';
-import DraftIcon from './Frame.svg';
-import ProfilePhoto from './Profile.svg';
-
-export {Logo, IconBack, HomeIcon, DraftIcon, ProfilePhoto};
